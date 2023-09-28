e9353043f0ee36947bb016aba7e68ea153e84d5d
Tahir Mehmood <tahir.meh012@gmail.com>
2023-09-28 15:46:23 +0500

    cerate a body-footer section

--- src/Components/Homepage/Body.js
+++ src/Components/Homepage/Body.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Body.css";
 import { Button } from "@mui/material";
+import { Image } from "@mui/icons-material";
 
 export default function Body() {
   return (
@@ -13,6 +14,17 @@ export default function Body() {
           </div>
           <div className="body-button">
             <Button>Explore Kitchen CollectionS</Button>
+          </div>
+        </div>
+        <div className="body-footer">
+          <div className="footer-btn">
+            <Button startIcon={<img src="" alt="icon" />}>DISCOVER MORE</Button>
+          </div>
+          <div className="elips-logo">
+            <img src="" alt="" />
+          </div>
+          <div className="message-icon">
+            <Image />
           </div>
         </div>
       </div>
